80b19ea1512760e0db9edceec6a22f19d8e4b48b
argon <4r6on55wydodo@gmail.com>
2025-03-17 16:33:54 +0700

    [NEW LOGIC] 3 palindrome

---
+++ 2-test-logic-junior-swap.js
@@ -0,0 +1,29 @@
+// 1. tukar nilai data dari variabel yang tersedia
+
+// function test(){
+// var a = 1
+// var b = 2
+// console.log('a awal = ', a)
+// console.log('b awal = ', b)
+
+// //code
+
+// console.log('b final = ', a)
+// console.log('a final = ', b)
+
+// test()
+
+function test() {
+  var a = 1;
+  var b = 2;
+  console.log("a awal = ", a);
+  console.log("b awal = ", b);
+
+  //code
+  //   gimana caranya nilai b jadi nilai a dan nilai a jadi nilai b
+
+  console.log("b final = ", a);
+  console.log("a final = ", b);
+}
+
+test();
